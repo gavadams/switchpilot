5359d65fd44fdb865438cfd96095851fa29abf40
gavadams <gav.adams@gmail.com>
2025-10-25 01:20:05 +0100

    fix: add admin components index and remove turbopack from build

---
+++ src/components/features/admin/index.ts
@@ -0,0 +1,7 @@
+export { default as BankDealRow } from './BankDealRow'
+export { default as ProductRow } from './ProductRow'
+export { default as AddBankDealAffiliateDialog } from './AddBankDealAffiliateDialog'
+export { default as AddProductDialog } from './AddProductDialog'
+export { default as DateRangeSelector } from './DateRangeSelector'
+export { default as PerformanceCard } from './PerformanceCard'
+
