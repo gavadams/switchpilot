a40e3a65401a486f6969c00ad8cb9584bfeacdda
gavadams <gav.adams@gmail.com>
2025-10-28 21:37:47 +0000

    ..

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,9 +4,10 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
   console.log('🔧 AdminLayout: Rendering')
 
   return (
-    <>
+    <div className="min-h-screen bg-background">
+      {/* Admin layout wrapper */}
       {children}
-    </>
+    </div>
   )
 }
 
